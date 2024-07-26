e94def02425d5e527e3be8985f79ce0ad0df5b3e
arronmccrory <arronmccrory@gmail.com>
2024-07-26 17:37:14 -0400

    added id to metrics script

--- app/components/Metrics/Metrics.tsx
+++ app/components/Metrics/Metrics.tsx
@@ -6,10 +6,12 @@ export default function Metrics() {
   return (
     <>
       <Script
+        id="google-analytics-init"
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
       <Script
+        id="google-analytics"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
