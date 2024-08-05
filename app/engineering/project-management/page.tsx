27fa0cf4d44b1122f46aaa3ce36fbe790334685e
Arron McCrory <arronmccrory@gmail.com>
2024-08-05 16:29:56 -0400

    updated project management

--- app/engineering/project-management/page.tsx
+++ app/engineering/project-management/page.tsx
@@ -1,9 +1,21 @@
 export default function Page() {
-    return (
-        <main>
-            <h1 className="text-4xl my-8">Project Management</h1>
-            <p>I believe that project management has less to do with telling people what to, and more to do with people telling me what to do.</p>
-            <p>What I mean is that morale and teamwork are what really matter. Steve Jobs once said <strong>"If you want to hire great people and have them stay working for you, you have to let them make a lot of decisions, and you have to be run by ideas, not hierarchy"</strong>. I believe once you develop a culture of trust and respect, you can achieve great things.</p>
-        </main>
-    )
-  }
+  return (
+    <main>
+      <h1 className="text-4xl my-8">Project Management</h1>
+      <p>
+        The best code I have seen has been created as a result of enthusiasm and
+        collaboration.
+      </p>
+      <p>
+        Morale and teamwork are what really matter. Steve Jobs once said{" "}
+        <strong>
+          "If you want to hire great people and have them stay working for you,
+          you have to let them make a lot of decisions, and you have to be run
+          by ideas, not hierarchy"
+        </strong>
+        . I believe once you develop a culture of trust and respect, you can
+        achieve great things.
+      </p>
+    </main>
+  );
+}
