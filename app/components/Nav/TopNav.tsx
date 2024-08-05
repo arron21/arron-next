0b794a64e1e8be558cdb00d3c26e3816db4e4311
Arron McCrory <arronmccrory@gmail.com>
2024-08-05 13:56:20 -0400

    reorg

--- app/components/Nav/TopNav.tsx
+++ app/components/Nav/TopNav.tsx
@@ -1,9 +1,9 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import Link from "next/link"
+import * as React from "react";
+import Link from "next/link";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -12,77 +12,70 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
   navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu"
-
-
+} from "@/components/ui/navigation-menu";
 
 export const engineeringItems = [
-    {
-        label: 'Frontend',
-        icon: 'pi pi-fw pi-upload',
-        url: '/engineering/frontend',
-        description: 'The stuff you do see'
-    },
-    {
-      label: 'Angular',
-      icon: 'pi pi-fw pi-upload',
-      url: '/engineering/angular',
-      description: 'An amazing framework'
-
+  {
+    label: "Frontend",
+    icon: "pi pi-fw pi-upload",
+    url: "/engineering/frontend",
+    description: "The stuff you do see",
   },
   {
-    label: 'React',
-    icon: 'pi pi-fw pi-upload',
-    url: '/engineering/react',
-    description: 'An overly popular framework'
-
+    label: "Angular",
+    icon: "pi pi-fw pi-upload",
+    url: "/engineering/angular",
+    description: "An amazing framework",
   },
   {
-    label: 'UI/UX',
-    icon: 'pi pi-fw pi-upload',
-    url: '/engineering/ui-ux',
-    description: 'More than just colors and fonts'
-
+    label: "React",
+    icon: "pi pi-fw pi-upload",
+    url: "/engineering/react",
+    description: "An overly popular framework",
   },
-    {
-        label: 'Backend',
-        icon: 'pi pi-fw pi-search',
-        url: '/engineering/backend',
-        description: "The stuff you don't see"
-
-    },
-    {
-      label: 'Design',
-      icon: 'pi pi-fw pi-search',
-      url: '/engineering/design',
-      description: 'A process that connects all pieces of a goal'
-
-    }
-  ]
-  
-
+  {
+    label: "UI/UX",
+    icon: "pi pi-fw pi-upload",
+    url: "/engineering/ui-ux",
+    description: "More than just colors and fonts",
+  },
+  {
+    label: "Backend",
+    icon: "pi pi-fw pi-search",
+    url: "/engineering/backend",
+    description: "The stuff you don't see",
+  },
+  {
+    label: "Design",
+    icon: "pi pi-fw pi-search",
+    url: "/engineering/design",
+    description: "A process that connects all pieces of a goal",
+  },
+  {
+    label: "Project Management",
+    url: "/engineering/project-management",
+    description: "You have to be run by ideas, not hierarchy.",
+  },
+];
 
 const components: { title: string; href: string; description: string }[] = [
   {
     title: "Photography",
-    href: "/skills/photography",
-    description:
-      "A picture is worth a thousand words.",
+    href: "/interests/photography",
+    description: "A picture is worth a thousand words.",
   },
-  {
-    title: "Project Management",
-    href: "/skills/project-management",
-    description:
-      "You have to be run by ideas, not hierarchy.",
-  },
-  
-]
+  // {
+  //   title: "Boxing",
+  //   href: "/interests/boxing",
+  //   description: "Pugilism, kinda sounds like a dog huh?",
+  // },
+];
 
 export default function TopNav() {
   return (
     <NavigationMenu className="my-4">
       <NavigationMenuList>
-      <NavigationMenuItem>
+        <NavigationMenuItem>
           <Link href="/" legacyBehavior passHref>
             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
               Home
@@ -113,7 +106,7 @@ export default function TopNav() {
           </NavigationMenuContent>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <NavigationMenuTrigger>Skills</NavigationMenuTrigger>
+          <NavigationMenuTrigger>Interests</NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
               {components.map((component) => (
@@ -128,10 +121,9 @@ export default function TopNav() {
             </ul>
           </NavigationMenuContent>
         </NavigationMenuItem>
-
       </NavigationMenuList>
     </NavigationMenu>
-  )
+  );
 }
 
 const ListItem = React.forwardRef<
@@ -145,7 +137,7 @@ const ListItem = React.forwardRef<
           ref={ref}
           className={cn(
             "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-            className
+            className,
           )}
           {...props}
         >
@@ -156,6 +148,6 @@ const ListItem = React.forwardRef<
         </a>
       </NavigationMenuLink>
     </li>
-  )
-})
-ListItem.displayName = "ListItem"
+  );
+});
+ListItem.displayName = "ListItem";
---
+++ app/interests/boxing/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+  return (
+    <main>
+      <h1 className="text-4xl my-8">Boxing</h1>
+      <p>Boxing has been an olympic sport since 688 BC.</p>
+      <p>
+        There is something inately basic to the sport of boxing. It can be seen
+        as just a vi
+      </p>
+    </main>
+  );
+}
