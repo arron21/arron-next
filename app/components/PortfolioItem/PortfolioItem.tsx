3adf1a42cc5d645ad05f9770fac837dcbb158cc0
Arron McCrory <arronmccrory@gmail.com>
2024-10-15 11:23:35 -0400

    change PortfolioItem to use next Image

--- app/components/PortfolioItem/PortfolioItem.tsx
+++ app/components/PortfolioItem/PortfolioItem.tsx
@@ -1,14 +1,16 @@
+import Image from "next/image";
+
 export default function PortfolioItem(props: any) {
   return (
     <div className="my-12 flex flex-col items-center justify-center w-full h-full">
       {props.title && <h3 className="text-2xl font-bold">{props.title}</h3>}
       {props.image && (
         <a href={props.link}>
-          <img
-            src={props.image}
-            alt="{props.alt}"
-            className="shadow-xl w-full h-full rounded-lg"
-          />
+        <Image
+          src={props.image}
+          alt="{props.alt}"
+          className="shadow-xl w-full h-full rounded-lg"
+        />
         </a>
       )}
     </div>
